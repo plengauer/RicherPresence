5ba9a85c4261e03bcf194a65df73bd4466c8f949
philipp.lengauer <p.lengauer@gmail.com>
2022-07-19 14:45:55 +0200

    self contained app

--- assemble.cmd
+++ assemble.cmd
@@ -1,5 +1,5 @@
-copy /Y run.cmd .\RicherPresence\bin\Release\net6.0\
-copy /Y RicherPresence.xml .\RicherPresence\bin\Release\net6.0\
-copy /Y ..\DXGIOutputDuplication\x64\Release\*.exe .\RicherPresence\bin\Release\net6.0\
-xcopy C:\"Program Files"\Tesseract-OCR\* .\RicherPresence\bin\Release\net6.0\ /Y /S
+copy /Y run.cmd .\RicherPresence\bin\Release\net6.0\win-x64\
+copy /Y RicherPresence.xml .\RicherPresence\bin\Release\net6.0\win-x64\
+copy /Y ..\DXGIOutputDuplication\x64\Release\*.exe .\RicherPresence\bin\Release\net6.0\win-x64\
+xcopy C:\"Program Files"\Tesseract-OCR\* .\RicherPresence\bin\Release\net6.0\win-x64\ /Y /S
 pause
